09f8fec71f87e422a237bee2c2ef3190c2f795a8
jschmitt <schmitty890@gmail.com>
2019-03-26 19:00:27 -0400

    init gatsby

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const Container = styled.div`
+  text-align: center;
+`;
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <Layout>
+    <SEO title="404: Not found" />
+    <Container>
+      <span
+        css={`
+          font-size: 10rem;
+          margin: 20vmin 0;
+          display: block;
+        `}
+        role="img"
+        aria-label="facepalm emoji"
+      >
+        🤦🏻‍♂️
+      </span>
+      <h1>NOT FOUND</h1>
+      <p>I have not added this page yet.</p>
+    </Container>
+  </Layout>
 );
 
 export default NotFoundPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,130 +1,68 @@
-import PropTypes from "prop-types";
-import React from "react";
+import React, { Component } from "react";
 import { graphql } from "gatsby";
-import { ThemeContext } from "../layouts";
-import Blog from "../components/Blog";
-import Hero from "../components/Hero";
-import Seo from "../components/Seo";
+import styled from "styled-components";
 
-class IndexPage extends React.Component {
-  separator = React.createRef();
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Bio from "../components/bio";
+import Post from "../components/post";
+import media from "../utils/media";
 
-  scrollToContent = e => {
-    this.separator.current.scrollIntoView({ block: "start", behavior: "smooth" });
-  };
+const Title = styled.h3`
+  font-weight: 800;
+  font-size: 2.6rem;
+  margin: 6rem 0 0;
 
+  ${media.phone`
+    margin: 3rem 0 0;
+  `}
+`;
+
+class BlogIndex extends Component {
   render() {
-    const {
-      data: {
-        posts: { edges: posts = [] },
-        bgDesktop: {
-          resize: { src: desktop }
-        },
-        bgTablet: {
-          resize: { src: tablet }
-        },
-        bgMobile: {
-          resize: { src: mobile }
-        },
-        site: {
-          siteMetadata: { facebook }
-        }
-      }
-    } = this.props;
-
-    const backgrounds = {
-      desktop,
-      tablet,
-      mobile
-    };
-
+    const { data } = this.props;
+    const posts = data.allMarkdownRemark.edges;
     return (
-      <React.Fragment>
-        <ThemeContext.Consumer>
-          {theme => (
-            <Hero scrollToContent={this.scrollToContent} backgrounds={backgrounds} theme={theme} />
-          )}
-        </ThemeContext.Consumer>
-
-        <hr ref={this.separator} />
-
-        <ThemeContext.Consumer>
-          {theme => <Blog posts={posts} theme={theme} />}
-        </ThemeContext.Consumer>
-
-        <Seo facebook={facebook} />
-
-        <style jsx>{`
-          hr {
-            margin: 0;
-            border: 0;
-          }
-        `}</style>
-      </React.Fragment>
+      <Layout>
+        <SEO title="All Posts" keywords={[`gatsby`, `blog`, `react`]} />
+        <Bio />
+        <main>
+          <Title>Latest Posts</Title>
+          {posts.map(({ node }) => {
+            return <Post key={node.id} node={node} />;
+          })}
+        </main>
+      </Layout>
     );
   }
 }
 
-IndexPage.propTypes = {
-  data: PropTypes.object.isRequired
-};
+export default BlogIndex;
 
-export default IndexPage;
-
-//eslint-disable-next-line no-undef
-export const query = graphql`
-  query IndexQuery {
-    posts: allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "//posts/[0-9]+.*--/" } }
-      sort: { fields: [fields___prefix], order: DESC }
-    ) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-            prefix
-          }
-          frontmatter {
-            title
-            category
-            author
-            cover {
-              children {
-                ... on ImageSharp {
-                  fluid(maxWidth: 800, maxHeight: 360) {
-                    ...GatsbyImageSharpFluid_withWebp
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+export const pageQuery = graphql`
+  query {
     site {
       siteMetadata {
-        facebook {
-          appId
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 160)
+          fields {
+            slug
+            readingTime {
+              text
+            }
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
         }
-      }
-    }
-    bgDesktop: imageSharp(fluid: { originalName: { regex: "/hero-background/" } }) {
-      resize(width: 1200, quality: 90, cropFocus: CENTER) {
-        src
-      }
-    }
-    bgTablet: imageSharp(fluid: { originalName: { regex: "/hero-background/" } }) {
-      resize(width: 800, height: 1100, quality: 90, cropFocus: CENTER) {
-        src
-      }
-    }
-    bgMobile: imageSharp(fluid: { originalName: { regex: "/hero-background/" } }) {
-      resize(width: 450, height: 850, quality: 90, cropFocus: CENTER) {
-        src
       }
     }
   }
 `;
-
-//hero-background
